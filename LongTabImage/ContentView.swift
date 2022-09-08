1f35f7be58b1afe3344d8128ff29fa273fad86b2
Taha Yasin KOKSAL <info@tahayasinkoksal.com.tr>
2022-09-08 23:00:11 +0300

    Initial Commit

---
+++ LongTabImage/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LongTabImage
+//
+//  Created by Taha Yasin Köksal on 8.09.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LongTabImage/LongTabImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  LongTabImageApp.swift
+//  LongTabImage
+//
+//  Created by Taha Yasin Köksal on 8.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct LongTabImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
